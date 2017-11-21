1027af9a6916a5e175934446d131f540cfa61af7
Asus <sashaabr>
2017-11-21 19:21:10 +0200

    Second commit

--- app/src/main/java/com/joyfullkiwi/converterlab/Activities/MainActivity.java
+++ app/src/main/java/com/joyfullkiwi/converterlab/Activities/MainActivity.java
@@ -1,9 +1,12 @@
-package com.joyfullkiwi.converterlab;
+package com.joyfullkiwi.converterlab.Activities;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.joyfullkiwi.converterlab.R;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
